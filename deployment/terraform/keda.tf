d2b4d8c301b9419f5faab3db9c7a2202e7ca2a3c
Alexis Langlet <alexis.langlet@etu.umontpellier.fr>
2024-04-25 17:00:11 +0200

    refacto: split tf in multiple files

---
+++ deployment/terraform/keda.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "keda" {
+  name             = "keda"
+  namespace        = "keda"
+  create_namespace = true
+  repository       = "https://kedacore.github.io/charts"
+  chart            = "keda"
+}
