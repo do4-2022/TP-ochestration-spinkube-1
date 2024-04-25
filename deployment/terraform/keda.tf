23089faf00f47bdee752e26b82bb98f4df1f105d
Alexis Langlet <alexis.langlet@etu.umontpellier.fr>
2024-04-25 17:00:32 +0200

    feat: add keda http add on

--- deployment/terraform/keda.tf
+++ deployment/terraform/keda.tf
@@ -5,3 +5,12 @@ resource "helm_release" "keda" {
   repository       = "https://kedacore.github.io/charts"
   chart            = "keda"
 }
+
+resource "helm_release" "keda_add_on_http" {
+  name             = "keda-http-add-on"
+  namespace        = "keda"
+  create_namespace = true
+  repository       = "https://kedacore.github.io/charts"
+  chart            = "keda-add-ons-http"
+  depends_on       = [helm_release.keda]
+}
